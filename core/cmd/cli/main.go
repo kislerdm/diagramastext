0369b4b3d16ea7976e5381d5cb323287aec59527
Dmitry Kisler <admin@dkisler.com>
2023-02-07 23:26:50 +0100

    chore: add illustrative entrypoint
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/cmd/cli/main.go
+++ core/cmd/cli/main.go
@@ -3,14 +3,33 @@ package main
 import (
 	"context"
 	"log"
+	"os"
 
 	"github.com/kislerdm/diagramastext/core"
 )
 
 func main() {
-	client := core.NewPlantUMLClient()
+	client, err := core.NewOpenAIClient(
+		core.ConfigOpenAI{
+			Token:       os.Getenv("OPENAI_API_KEY"),
+			MaxTokens:   768,
+			Temperature: 0,
+		},
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
 
-	svg, err := client.Do(context.Background(), core.DiagramGraph{Nodes: []*core.Node{{ID: "0"}}})
+	graph, err := client.Do(
+		context.Background(),
+		`c4 diagram with kotlin backend reading postgres`,
+	)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	plantUMLClient := core.NewPlantUMLClient()
+	svg, err := plantUMLClient.Do(context.Background(), graph)
 	if err != nil {
 		log.Fatalln(err)
 	}
