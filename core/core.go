20649a4153d5b595cec9561862aba4fbacef58c3
Dmitry Kisler <admin@dkisler.com>
2023-02-07 20:13:12 +0100

    Logic to convert openAI output to the PlantUML code (#20)
    * feat(handler.go): adds handler's sceleton.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat(handler.go): improves test.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * fix(handler.go): fixes ClientPlantUML signature methods.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat(core.go): add graph to planUML code converter sketch.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: add graph2plantuml code logic.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: add graph2plantuml code logic.
    chore: refactors logic to have a clear split of interfaces: input -> graph -> svg -> output.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * fix: adjust http interface to follow the client interface pattern
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: add test for `linkDirection`
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: add test for `diagramLink2UML`.
    fix: add error handling.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: add test for `diagramLink2UML`.
    fix: add error handling.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: add `encode6bit` tests
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: improve coverage
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: improve coverage
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: improve coverage
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: add var variable to overwrite http client.
    chore: tests coverage improved.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: improved test coverage.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: add system boundary logic.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: cleaup
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: wraps diagram as code logic.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat: sets SOLID internal contract between interfaces' clients.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * feat (c4diagram): implemented reviewer's suggestions.
    
    https://github.com/kislerdm/diagramastext/pull/20/files/a8172589a31eda09b9a51c748d9b29b2fe985eb9#r1097979251;
    https://github.com/kislerdm/diagramastext/pull/20/files/a8172589a31eda09b9a51c748d9b29b2fe985eb9#r1097962062;
    https://github.com/kislerdm/diagramastext/pull/20/files/a8172589a31eda09b9a51c748d9b29b2fe985eb9#r1097971714;
    https://github.com/kislerdm/diagramastext/pull/20/files/a8172589a31eda09b9a51c748d9b29b2fe985eb9#r1098017415.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    * chore: add test runner.
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>
    
    ---------
    
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/core.go
+++ core/core.go
@@ -1,189 +1,55 @@
 package core
 
 import (
-	"bytes"
-	"errors"
-	"io"
+	"context"
 	"net/http"
-	"strconv"
-	"time"
-	"unsafe"
-
-	"github.com/kislerdm/diagramastext/core/compression"
 )
+
+// DiagramGraph defines the diagram graph.
+type DiagramGraph struct {
+	Title  string  `json:"title"`
+	Footer string  `json:"footer"`
+	Nodes  []*Node `json:"nodes"`
+	Links  []*Link `json:"links"`
+}
+
+// Node diagram's definition node.
+type Node struct {
+	ID         string `json:"id"`
+	Label      string `json:"label"`
+	Group      string `json:"group"`
+	Technology string `json:"technology"`
+	External   bool   `json:"external"`
+	IsQueue    bool   `json:"is_queue"`
+	IsDatabase bool   `json:"is_database"`
+}
+
+// Link diagram's definition link.
+type Link struct {
+	From       string `json:"from"`
+	To         string `json:"to"`
+	Direction  string `json:"direction"`
+	Label      string `json:"label"`
+	Technology string `json:"technology"`
+}
+
+// ResponseDiagram response object.
+type ResponseDiagram interface {
+	// MustMarshal serialises the result as JSON.
+	MustMarshal() []byte
+}
+
+// ClientInputToGraph client to convert user input inquiry to the DiagramGraph.
+type ClientInputToGraph interface {
+	Do(context.Context, string) (DiagramGraph, error)
+}
+
+// ClientGraphToDiagram client to convert DiagramGraph to diagram artifact, e.g. svg image.
+type ClientGraphToDiagram interface {
+	Do(context.Context, DiagramGraph) (ResponseDiagram, error)
+}
 
 // HttpClient http base client.
 type HttpClient interface {
-	Get(url string) (resp *http.Response, err error)
-}
-
-type options struct {
-	httpClient HttpClient
-}
-
-// PlantUMLClient client to communicate with the plantuml server.
-type PlantUMLClient interface {
-	// GenerateSVG generates the SVG diagram using the diagram as code input.
-	GenerateSVG(code string) ([]byte, error)
-
-	// GeneratePNG generates the PNG diagram using the diagram as code input.
-	GeneratePNG(code string) ([]byte, error)
-}
-
-type client struct {
-	options options
-
-	baseURL string
-}
-
-func (c *client) GenerateSVG(code string) ([]byte, error) {
-	p, err := code2Path(code)
-	if err != nil {
-		return nil, err
-	}
-	return c.requestHandler("svg/" + p)
-}
-
-func (c *client) GeneratePNG(code string) ([]byte, error) {
-	p, err := code2Path(code)
-	if err != nil {
-		return nil, err
-	}
-	return c.requestHandler("png/" + p)
-}
-
-func (c *client) requestHandler(p string) ([]byte, error) {
-	resp, err := c.options.httpClient.Get(c.baseURL + p)
-	if err != nil {
-		return nil, err
-	}
-
-	if resp.StatusCode > 209 {
-		return nil, errors.New("error status code: " + strconv.Itoa(resp.StatusCode))
-	}
-
-	buf, err := io.ReadAll(resp.Body)
-	defer func() { _ = resp.Body.Close() }()
-	if err != nil {
-		return nil, err
-	}
-
-	return buf, nil
-}
-
-const (
-	baseURL        = "https://www.plantuml.com/plantuml/"
-	defaultTimeout = 1 * time.Minute
-)
-
-// NewPlantUMLClient initiates the client to communicate with the plantuml server.
-func NewPlantUMLClient(optFns ...func(*options)) PlantUMLClient {
-	o := options{
-		httpClient: nil,
-	}
-
-	for _, fn := range optFns {
-		fn(&o)
-	}
-
-	resolveHTTPClient(&o)
-
-	return &client{
-		options: o,
-		baseURL: baseURL,
-	}
-}
-
-func resolveHTTPClient(o *options) {
-	if o.httpClient == nil {
-		o.httpClient = &http.Client{Timeout: defaultTimeout}
-	}
-}
-
-// code2Path converts the diagram as code to the 64Bytes encoded string to query plantuml
-//
-// Example: the diagram's code
-// @startuml
-//
-//	a -> b
-//
-// @enduml
-//
-// will be converted to SoWkIImgAStDuL80WaG5NJk592w7rBmKe100
-//
-// The resulting string to be used to generate C4 diagram
-// - as png: GET www.plantuml.com/plantuml/png/SoWkIImgAStDuL80WaG5NJk592w7rBmKe100
-// - as svg: GET www.plantuml.com/plantuml/svg/SoWkIImgAStDuL80WaG5NJk592w7rBmKe100
-func code2Path(s string) (string, error) {
-	zb, err := compress(*(*[]byte)(unsafe.Pointer(&s)))
-	if err != nil {
-		return "", err
-	}
-	return encode64(zb), nil
-}
-
-func compress(v []byte) ([]byte, error) {
-	var options = compression.DefaultOptions()
-	var w bytes.Buffer
-	if err := compression.Compress(&options, compression.FORMAT_DEFLATE, v, &w); err != nil {
-		return nil, err
-	}
-	return w.Bytes(), nil
-}
-
-func encode64(e []byte) string {
-	var r bytes.Buffer
-	for i := 0; i < len(e); i += 3 {
-		switch len(e) {
-		case i + 2:
-			r.Write(append3bytes(e[i], e[i+1], 0))
-		case i + 1:
-			r.Write(append3bytes(e[i], 0, 0))
-		default:
-			r.Write(append3bytes(e[i], e[i+1], e[i+2]))
-		}
-	}
-	return r.String()
-}
-
-func append3bytes(e, n, t byte) []byte {
-	c1 := e >> 2
-	c2 := (3&e)<<4 | n>>4
-	c3 := (15&n)<<2 | t>>6
-	c4 := 63 & t
-
-	var buf bytes.Buffer
-
-	buf.WriteByte(encode6bit(c1 & 63))
-	buf.WriteByte(encode6bit(c2 & 63))
-	buf.WriteByte(encode6bit(c3 & 63))
-	buf.WriteByte(encode6bit(c4 & 63))
-
-	return buf.Bytes()
-}
-
-func encode6bit(e byte) byte {
-	if e < 10 {
-		return 48 + e
-	}
-
-	e -= 10
-	if e < 26 {
-		return 65 + e
-	}
-
-	e -= 26
-	if e < 26 {
-		return 97 + e
-	}
-
-	e -= 26
-	switch e {
-	case 0:
-		return '-'
-	case 1:
-		return '_'
-	default:
-		return '?'
-	}
+	Do(req *http.Request) (resp *http.Response, err error)
 }
