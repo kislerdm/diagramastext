6979bc7952333a40cdfabb05d21899e7d97de182
Dmitry Kisler <admin@dkisler.com>
2023-02-09 15:43:41 +0100

    chore(lambda): improve test coverage for the lambda handler.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/cmd/lambda/handler_test.go
+++ core/cmd/lambda/handler_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"math/rand"
 	"net/http"
 	"reflect"
+	"strconv"
 	"testing"
 	"time"
 
@@ -194,6 +196,173 @@ func Test_parseClientError(t *testing.T) {
 			tt.name, func(t *testing.T) {
 				if got := parseClientError(tt.args.err); !reflect.DeepEqual(got, tt.want) {
 					t.Errorf("parseClientError() = %v, want %v", got, tt.want)
+				}
+			},
+		)
+	}
+}
+
+type mockClientInputToGraph struct {
+	Diagram core.DiagramGraph
+	Err     error
+}
+
+func (m mockClientInputToGraph) Do(ctx context.Context, s string) (core.DiagramGraph, error) {
+	_ = ctx
+	_ = s
+	return m.Diagram, m.Err
+}
+
+type mockClientGraphToDiagram struct {
+	Resp core.ResponseDiagram
+	Err  error
+}
+
+func (m mockClientGraphToDiagram) Do(ctx context.Context, graph core.DiagramGraph) (core.ResponseDiagram, error) {
+	_ = ctx
+	_ = graph
+	return m.Resp, m.Err
+}
+
+func Test_handler(t *testing.T) {
+	type fields struct {
+		clientModel   core.ClientInputToGraph
+		clientDiagram core.ClientGraphToDiagram
+	}
+	type args struct {
+		ctx context.Context
+		req events.APIGatewayProxyRequest
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    events.APIGatewayProxyResponse
+		wantErr bool
+	}{
+		{
+			name: "happy path",
+			fields: fields{
+				clientModel: mockClientInputToGraph{
+					Diagram: core.DiagramGraph{
+						Nodes: []*core.Node{{ID: "0"}},
+					},
+					Err: nil,
+				},
+				clientDiagram: mockClientGraphToDiagram{
+					Resp: core.ResponseC4Diagram{SVG: "<svg></svg>"},
+					Err:  nil,
+				},
+			},
+			args: args{
+				ctx: context.TODO(),
+				req: events.APIGatewayProxyRequest{
+					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+				},
+			},
+			want: events.APIGatewayProxyResponse{
+				StatusCode: http.StatusOK,
+				Body:       string(core.ResponseC4Diagram{SVG: "<svg></svg>"}.MustMarshal()),
+			},
+			wantErr: false,
+		},
+		{
+			name: "unhappy path: faulty prompt",
+			args: args{
+				ctx: context.TODO(),
+				req: events.APIGatewayProxyRequest{
+					Body: `{"prompt":`,
+				},
+			},
+			want: events.APIGatewayProxyResponse{
+				StatusCode: http.StatusUnprocessableEntity,
+				Body:       "could not recognise the prompt format",
+			},
+			wantErr: true,
+		},
+		{
+			name: "unhappy path: invalid prompt",
+			args: args{
+				ctx: context.TODO(),
+				req: events.APIGatewayProxyRequest{
+					Body: `{"prompt":"` + randomString(promptLengthMin-1) + `"}`,
+				},
+			},
+			want: events.APIGatewayProxyResponse{
+				StatusCode: http.StatusBadRequest,
+				Body: "prompt length must be between " + strconv.Itoa(promptLengthMin) + " and " +
+					strconv.Itoa(promptLengthMax) + " characters",
+			},
+			wantErr: true,
+		},
+		{
+			name: "unhappy path: openAI error",
+			fields: fields{
+				clientModel: mockClientInputToGraph{
+					Err: core.Error{
+						Service:                   core.ServiceOpenAI,
+						Stage:                     core.StageResponse,
+						Message:                   "foobar",
+						ServiceResponseStatusCode: http.StatusTooManyRequests,
+					},
+				},
+			},
+			args: args{
+				ctx: context.TODO(),
+				req: events.APIGatewayProxyRequest{
+					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+				},
+			},
+			want: events.APIGatewayProxyResponse{
+				StatusCode: http.StatusTooManyRequests,
+				Body:       "service experiences high load, please try later",
+			},
+			wantErr: true,
+		},
+		{
+			name: "unhappy path: plantUML error",
+			fields: fields{
+				clientModel: mockClientInputToGraph{
+					Diagram: core.DiagramGraph{
+						Nodes: []*core.Node{{ID: "0"}},
+					},
+					Err: nil,
+				},
+				clientDiagram: mockClientGraphToDiagram{
+					Err: core.Error{
+						Service:                   core.ServiePlantUML,
+						Stage:                     core.StageResponse,
+						Message:                   "foobar",
+						ServiceResponseStatusCode: http.StatusTooManyRequests,
+					},
+				},
+			},
+			args: args{
+				ctx: context.TODO(),
+				req: events.APIGatewayProxyRequest{
+					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+				},
+			},
+			want: events.APIGatewayProxyResponse{
+				StatusCode: http.StatusTooManyRequests,
+				Body:       "service experiences high load, please try later",
+			},
+			wantErr: true,
+		},
+	}
+
+	t.Parallel()
+
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				got, gotErr := handler(tt.fields.clientModel, tt.fields.clientDiagram)(tt.args.ctx, tt.args.req)
+				if (gotErr != nil) != tt.wantErr {
+					t.Errorf("handler execution error = %v, wantErr %v", gotErr, tt.wantErr)
+					return
+				}
+				if !reflect.DeepEqual(got, tt.want) {
+					t.Errorf("handler execution got: %v, want: %v", got, tt.want)
 				}
 			},
 		)
