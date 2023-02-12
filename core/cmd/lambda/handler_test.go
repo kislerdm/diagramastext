064c1e1ac78f1b00b736beb103d510bf13c4d343
Dmitry Kisler <admin@dkisler.com>
2023-02-12 15:23:11 +0100

    chore(core): refactoring - handler is moved from lambda to be reused for means of deployment other than aws lambda.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/cmd/lambda/handler_test.go
+++ core/cmd/lambda/handler_test.go
@@ -11,117 +11,8 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/kislerdm/diagramastext/core"
+	coreHandler "github.com/kislerdm/diagramastext/core/handler"
 )
-
-func Test_readPrompt(t *testing.T) {
-	type args struct {
-		req events.APIGatewayProxyRequest
-	}
-	tests := []struct {
-		name    string
-		args    args
-		want    string
-		wantErr bool
-	}{
-		{
-			name: "happy path",
-			args: args{
-				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt":"foobar"}`,
-				},
-			},
-			want:    "foobar",
-			wantErr: false,
-		},
-		{
-			name: "unhappy path: base64 encoded",
-			args: args{
-				req: events.APIGatewayProxyRequest{
-					Body:            `eyJwcm9tcHQiOiJmb29iYXIifQ==`,
-					IsBase64Encoded: true,
-				},
-			},
-			want:    "",
-			wantErr: true,
-		},
-		{
-			name: "unhappy path: corrupt data",
-			args: args{
-				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt":"foobar"`,
-				},
-			},
-			want:    "",
-			wantErr: true,
-		},
-		{
-			name: "unhappy path: corrupt data",
-			args: args{
-				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt":"foobar"`,
-				},
-			},
-			want:    "",
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				got, err := readPrompt(tt.args.req)
-				if (err != nil) != tt.wantErr {
-					t.Errorf("readPrompt() error = %v, wantErr %v", err, tt.wantErr)
-					return
-				}
-				if got != tt.want {
-					t.Errorf("readPrompt() got = %v, want %v", got, tt.want)
-				}
-			},
-		)
-	}
-}
-
-func Test_validatePrompt(t *testing.T) {
-	type args struct {
-		prompt string
-	}
-	tests := []struct {
-		name    string
-		args    args
-		wantErr bool
-	}{
-		{
-			name: "valid",
-			args: args{
-				prompt: "c4 diagram with Go backend reading postgres",
-			},
-			wantErr: false,
-		},
-		{
-			name: "invalid: short",
-			args: args{
-				prompt: randomString(promptLengthMin - 1),
-			},
-			wantErr: true,
-		},
-		{
-			name: "invalid: long",
-			args: args{
-				prompt: randomString(promptLengthMax + 1),
-			},
-			wantErr: true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(
-			tt.name, func(t *testing.T) {
-				if err := validatePrompt(tt.args.prompt); (err != nil) != tt.wantErr {
-					t.Errorf("validatePrompt() error = %v, wantErr %v", err, tt.wantErr)
-				}
-			},
-		)
-	}
-}
 
 func randomString(length int) string {
 	const charset = "abcdef"
@@ -266,7 +157,7 @@ func Test_handler(t *testing.T) {
 			args: args{
 				ctx: context.TODO(),
 				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+					Body: `{"prompt": "` + randomString(coreHandler.PromptLengthMin+1) + `"}`,
 				},
 			},
 			want: events.APIGatewayProxyResponse{
@@ -302,14 +193,14 @@ func Test_handler(t *testing.T) {
 			args: args{
 				ctx: context.TODO(),
 				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt":"` + randomString(promptLengthMin-1) + `"}`,
+					Body: `{"prompt":"` + randomString(coreHandler.PromptLengthMin-1) + `"}`,
 				},
 			},
 			want: events.APIGatewayProxyResponse{
 				Headers:    expectedHandler,
 				StatusCode: http.StatusBadRequest,
-				Body: "prompt length must be between " + strconv.Itoa(promptLengthMin) + " and " +
-					strconv.Itoa(promptLengthMax) + " characters",
+				Body: "prompt length must be between " + strconv.Itoa(coreHandler.PromptLengthMin) + " and " +
+					strconv.Itoa(coreHandler.PromptLengthMax) + " characters",
 			},
 			wantErr: true,
 		},
@@ -329,7 +220,7 @@ func Test_handler(t *testing.T) {
 			args: args{
 				ctx: context.TODO(),
 				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+					Body: `{"prompt": "` + randomString(100) + `"}`,
 				},
 			},
 			want: events.APIGatewayProxyResponse{
@@ -361,7 +252,7 @@ func Test_handler(t *testing.T) {
 			args: args{
 				ctx: context.TODO(),
 				req: events.APIGatewayProxyRequest{
-					Body: `{"prompt": "` + randomString(promptLengthMin+1) + `"}`,
+					Body: `{"prompt": "` + randomString(100) + `"}`,
 				},
 			},
 			want: events.APIGatewayProxyResponse{
--- core/cmd/lambda/main.go
+++ core/cmd/lambda/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/kislerdm/diagramastext/core"
+	coreHandler "github.com/kislerdm/diagramastext/core/handler"
 )
 
 func main() {
@@ -40,4 +44,80 @@ func mustParseInt(s string) int {
 func mustParseFloat32(s string) float32 {
 	o, _ := strconv.ParseFloat(s, 10)
 	return float32(o)
+}
+
+type corsHeaders map[string]string
+
+func (h corsHeaders) setHeaders(resp events.APIGatewayProxyResponse) events.APIGatewayProxyResponse {
+	if h == nil {
+		return resp
+	}
+
+	if resp.Headers == nil {
+		resp.Headers = map[string]string{}
+	}
+
+	for k, v := range h {
+		resp.Headers[k] = v
+
+		if k == "Access-Control-Allow-Origin" && (v == "" || v == "'*'") {
+			resp.Headers[k] = "*"
+		}
+	}
+
+	return resp
+}
+
+func handler(
+	clientModel core.ClientInputToGraph, clientDiagram core.ClientGraphToDiagram, corsHeaders corsHeaders,
+) func(
+	ctx context.Context, req events.APIGatewayProxyRequest,
+) (events.APIGatewayProxyResponse, error) {
+	return func(
+		ctx context.Context, req events.APIGatewayProxyRequest,
+	) (events.APIGatewayProxyResponse, error) {
+		prompt, err := coreHandler.ReadPrompt([]byte(req.Body))
+		if err != nil {
+			return corsHeaders.setHeaders(
+				events.APIGatewayProxyResponse{
+					StatusCode: http.StatusUnprocessableEntity,
+					Body:       "could not recognise the prompt format",
+				},
+			), err
+		}
+
+		if err := coreHandler.ValidatePrompt(prompt); err != nil {
+			return corsHeaders.setHeaders(
+				events.APIGatewayProxyResponse{
+					StatusCode: http.StatusBadRequest,
+					Body:       err.Error(),
+				},
+			), err
+		}
+
+		graph, err := clientModel.Do(ctx, prompt)
+		if err != nil {
+			return corsHeaders.setHeaders(parseClientError(err)), err
+		}
+
+		svg, err := clientDiagram.Do(ctx, graph)
+		if err != nil {
+			return corsHeaders.setHeaders(parseClientError(err)), err
+		}
+
+		return corsHeaders.setHeaders(
+			events.APIGatewayProxyResponse{
+				StatusCode: http.StatusOK,
+				Body:       string(svg.MustMarshal()),
+			},
+		), err
+	}
+}
+
+func parseClientError(err error) events.APIGatewayProxyResponse {
+	v := coreHandler.ParseClientError(err)
+	return events.APIGatewayProxyResponse{
+		StatusCode: v.StatusCode,
+		Body:       string(v.Body),
+	}
 }
