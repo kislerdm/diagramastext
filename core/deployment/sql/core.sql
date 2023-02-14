e69c3d3a5fa35080d6f88a6ec0de15f46d8f1cd8
Dmitry Kisler <admin@dkisler.com>
2023-02-14 01:05:40 +0100

    chore(db): add db provisioning to store users' requests.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/deployment/sql/core.sql
+++ core/deployment/sql/core.sql
@@ -1,17 +1,17 @@
-CREATE EXTENSION "uuid-ossp";
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE IF NOT EXISTS user_prompt (
-    id SERIAL8,
-    request_id TEXT,
-    user_id TEXT,
-    prompt TEXT,
-    timestamp TIMESTAMP
+    id         SERIAL8,
+    request_id TEXT NOT NULL,
+    user_id    TEXT NOT NULL DEFAULT 'noAuthN',
+    prompt     TEXT NOT NULL ,
+    timestamp  TIMESTAMP NOT NULL DEFAULT NOW()
 );
 
 CREATE TABLE IF NOT EXISTS openai_response (
-   id SERIAL8,
-   request_id TEXT,
-   user_id TEXT,
-   response TEXT,
-   timestamp TIMESTAMP
+    id         SERIAL8,
+    request_id TEXT NOT NULL,
+    user_id    TEXT NOT NULL DEFAULT 'noAuthN',
+    response   TEXT NOT NULL ,
+    timestamp  TIMESTAMP NOT NULL DEFAULT NOW()
 );
