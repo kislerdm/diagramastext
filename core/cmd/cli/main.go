c55dc99d55b3c40d67107045e0be17153a2b6af4
Dmitry Kisler <admin@dkisler.com>
2023-02-16 14:45:17 +0100

    chore(core): adjust app for local tests
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- core/cmd/cli/main.go
+++ core/cmd/cli/main.go
@@ -5,8 +5,12 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"os"
+	"time"
+
+	"github.com/kislerdm/diagramastext/core/storage"
 
 	"github.com/kislerdm/diagramastext/core"
 )
@@ -23,12 +27,39 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	graph, err := client.Do(
+	clientStorage, err := storage.NewClient(
 		context.Background(),
-		`c4 diagram with kotlin backend reading postgres`,
+		os.Getenv("NEON_HOST"),
+		os.Getenv("NEON_DBNAME"),
+		os.Getenv("NEON_USER"),
+		os.Getenv("NEON_PASSWORD"),
 	)
 	if err != nil {
 		log.Fatalln(err)
+	}
+
+	const prompt = `c4 diagram with kotlin backend reading postgres`
+	//if err := clientStorage.WritePrompt(context.Background(), userInput); err != nil {
+	//	log.Print("WritePrompt() error " + err.Error())
+	//}
+
+	graph, err := client.Do(context.Background(), prompt)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, _ := json.Marshal(graph)
+
+	predictionOutput := core.ModelOutput{
+		CallID: core.CallID{
+			RequestID: "6863f332-1809-40ec-8bbd-878ccb11ff06",
+			UserID:    "00000000-0000-0000-0000-000000000001",
+		},
+		Response:  string(resp),
+		Timestamp: time.Now().UTC(),
+	}
+	if err := clientStorage.WriteModelPrediction(context.Background(), predictionOutput); err != nil {
+		log.Print("WriteModelPrediction() error " + err.Error())
 	}
 
 	plantUMLClient := core.NewPlantUMLClient()
