c0ed3735834dca191cf7321767a6418fd7642251
Dmitry Kisler <admin@dkisler.com>
2023-03-07 07:19:14 +0100

    chore(rendering): decouple rendering logic
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

---
+++ server/core/rendering/plantuml/utils.go
@@ -0,0 +1,11 @@
+package plantuml
+
+import "strings"
+
+func stringCleaner(s string) string {
+	s = strings.TrimSpace(s)
+	s = strings.TrimLeft(s, `"`)
+	s = strings.TrimRight(s, `"`)
+	s = strings.ReplaceAll(s, "\n", "\\n")
+	return s
+}
