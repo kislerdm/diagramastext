25ea27dd63cad6f13a087a6ceaf3e492588824dc
Dmitry Kisler <admin@dkisler.com>
2023-03-15 13:57:11 +0100

    chore(gcpsecretsmanager): add secretsmanager pkg.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

---
+++ server/core/pkg/gcpsecretsmanager/secretsmanager.go
@@ -0,0 +1,75 @@
+package gcpsecretsmanager
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"hash/crc32"
+
+	secretsmanager "cloud.google.com/go/secretmanager/apiv1"
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+	"github.com/googleapis/gax-go/v2"
+	"google.golang.org/api/option"
+)
+
+// NewSecretmanager initialises the GCP gcpsecretsmanager Client.
+func NewSecretmanager(ctx context.Context, opts ...option.ClientOption) (
+	*Client, error,
+) {
+	c, err := secretsmanager.NewClient(ctx, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{c}, nil
+}
+
+// Client gcpsecretsmanager client.
+type Client struct {
+	c gcpSecretsmanagerClient
+}
+
+func (c Client) ReadLastVersion(ctx context.Context, uri string, output interface{}) error {
+	res, err := c.c.AccessSecretVersion(
+		ctx, &secretmanagerpb.AccessSecretVersionRequest{
+			Name: uri,
+		},
+	)
+	if err != nil {
+		return err
+	}
+
+	if err := isValidResponse(res); err != nil {
+		return err
+	}
+
+	return json.Unmarshal(res.Payload.Data, output)
+}
+
+func isValidResponse(res *secretmanagerpb.AccessSecretVersionResponse) error {
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	checksum := int64(crc32.Checksum(res.GetPayload().GetData(), crc32c))
+	if checksum != res.GetPayload().GetDataCrc32C() {
+		return errors.New("data corruption detected")
+	}
+	return nil
+}
+
+type gcpSecretsmanagerClient interface {
+	AccessSecretVersion(
+		ctx context.Context, req *secretmanagerpb.AccessSecretVersionRequest, opts ...gax.CallOption,
+	) (*secretmanagerpb.AccessSecretVersionResponse, error)
+}
+
+type mockGCPSecretsmanagerClient struct {
+	v   *secretmanagerpb.AccessSecretVersionResponse
+	err error
+}
+
+func (m mockGCPSecretsmanagerClient) AccessSecretVersion(
+	_ context.Context, _ *secretmanagerpb.AccessSecretVersionRequest, _ ...gax.CallOption,
+) (*secretmanagerpb.AccessSecretVersionResponse, error) {
+	if m.err != nil {
+		return nil, m.err
+	}
+	return m.v, nil
+}
---
+++ server/core/pkg/gcpsecretsmanager/secretsmanager_test.go
@@ -0,0 +1,181 @@
+package gcpsecretsmanager
+
+import (
+	"context"
+	_ "embed"
+	"errors"
+	"hash/crc32"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/secretmanager/apiv1/secretmanagerpb"
+)
+
+//go:embed dummy-key.json
+var key []byte
+
+func TestNewSecretmanager(t *testing.T) {
+	t.Parallel()
+
+	t.Run(
+		"happy path", func(t *testing.T) {
+			// GIVEN
+			// The credentials json key
+			dir := t.TempDir()
+			p := dir + "/google-key.json"
+			if err := os.WriteFile(p, key, 0777); err != nil {
+				t.Fatal(err)
+			}
+			t.Setenv("GOOGLE_APPLICATION_CREDENTIALS", p)
+			// WHEN
+			_, err := NewSecretmanager(context.TODO())
+
+			// THEN
+			if err != nil {
+				t.Errorf("NewSecretmanager(): no error expected")
+				return
+			}
+		},
+	)
+
+	t.Run(
+		"unhappy path", func(t *testing.T) {
+			// GIVEN
+			// No credentials
+			// WHEN
+			_, err := NewSecretmanager(context.TODO())
+
+			// THEN
+			if err == nil {
+				t.Errorf("NewSecretmanager(): error expected")
+				return
+			}
+		},
+	)
+}
+
+type secret struct {
+	Foo string `json:"foo"`
+}
+
+func probeChecksum(v []byte) *int64 {
+	crc32c := crc32.MakeTable(crc32.Castagnoli)
+	o := int64(crc32.Checksum(v, crc32c))
+	return &o
+}
+
+func TestClient_ReadLastVersionHappyPath(t *testing.T) {
+	t.Run(
+		"happy path", func(t *testing.T) {
+			// GIVEN
+			client := Client{
+				c: mockGCPSecretsmanagerClient{
+					v: &secretmanagerpb.AccessSecretVersionResponse{
+						Name: "foo",
+						Payload: &secretmanagerpb.SecretPayload{
+							Data:       []byte(`{"foo":"bar"}`),
+							DataCrc32C: probeChecksum([]byte(`{"foo":"bar"}`)),
+						},
+					},
+				},
+			}
+
+			// WHEN
+			var s secret
+			err := client.ReadLastVersion(context.TODO(), "foo", &s)
+
+			// THEN
+			if err != nil {
+				t.Fatalf("unexpected error: %+v", err)
+			}
+
+			if s.Foo != "bar" {
+				t.Fatalf("unexpected secret value")
+			}
+		},
+	)
+}
+
+func TestClient_ReadLastVersionUnhappyPaths(t *testing.T) {
+	type fields struct {
+		c gcpSecretsmanagerClient
+	}
+	type args struct {
+		ctx    context.Context
+		uri    string
+		output interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		{
+			name: "missing secret",
+			fields: fields{
+				c: mockGCPSecretsmanagerClient{
+					v:   nil,
+					err: errors.New("secret 'foo' is missing"),
+				},
+			},
+			args: args{
+				ctx:    context.TODO(),
+				uri:    "foo",
+				output: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "wrong payload checksum",
+			fields: fields{
+				c: mockGCPSecretsmanagerClient{
+					v: &secretmanagerpb.AccessSecretVersionResponse{
+						Name: "foo",
+						Payload: &secretmanagerpb.SecretPayload{
+							Data: []byte(`{"foo":"bar"}`),
+						},
+					},
+				},
+			},
+			args: args{
+				ctx:    context.TODO(),
+				uri:    "foo",
+				output: nil,
+			},
+			wantErr: true,
+		},
+		{
+			name: "wrong payload encoding format",
+			fields: fields{
+				c: mockGCPSecretsmanagerClient{
+					v: &secretmanagerpb.AccessSecretVersionResponse{
+						Name: "foo",
+						Payload: &secretmanagerpb.SecretPayload{
+							Data:       []byte(`"foo":"bar"`),
+							DataCrc32C: probeChecksum([]byte(`"foo":"bar"`)),
+						},
+					},
+				},
+			},
+			args: args{
+				ctx:    context.TODO(),
+				uri:    "foo",
+				output: nil,
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(
+			tt.name, func(t *testing.T) {
+				c := Client{
+					c: tt.fields.c,
+				}
+				if err := c.ReadLastVersion(tt.args.ctx, tt.args.uri, tt.args.output); (err != nil) != tt.wantErr {
+					t.Errorf("ReadLastVersion() error = %v, wantErr %v", err, tt.wantErr)
+				}
+			},
+		)
+	}
+}
