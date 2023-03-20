88e64ffdffa1933c5a25ef55e99464928d3bb9a7
Dmitry Kisler <admin@dkisler.com>
2023-03-20 16:07:07 +0100

    chore(tf-prod): deploy core to the gcp prod project.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- server/deployment/gcp/prod/main.tf
+++ server/deployment/gcp/prod/main.tf
@@ -21,20 +21,17 @@ data "google_project" "project" {}
 variable "imagetag" {
   type        = string
   description = "Docker image tag."
-  default     = ""
 }
 
 module "core" {
-  source   = "../stack"
-  project  = "diagramastext-prod"
-  imagetag = var.imagetag
+  source     = "../stack"
+  project    = "diagramastext-prod"
+  imagetag   = var.imagetag
+  location   = "us-central1"
+  api_domain = "api.diagramastext.dev"
   cors_headers = {
     "Access-Control-Allow-Origin"  = "https://diagramastext.dev"
     "Access-Control-Allow-Headers" = "Content-Type,X-Amz-Date,x-api-key,Authorization,X-Api-Key,X-Amz-Security-Token"
     "Access-Control-Allow-Methods" = "POST,OPTIONS"
   }
-}
-
-output "core_url" {
-  value = module.core.core_url
 }
