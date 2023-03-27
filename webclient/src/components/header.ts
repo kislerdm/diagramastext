e74c50088f7fad6598780dedc7caf271d5e3a036
Dmitry Kisler <admin@dkisler.com>
2023-03-27 19:09:40 +0200

    chore(weclient): header refactoring.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- webclient/src/components/header.ts
+++ webclient/src/components/header.ts
@@ -1,7 +1,8 @@
-import style from './header.module.css';
+// @ts-ignore
+import {title} from './header.module.css';
 
-const Header = `<header>
-    <div class="${style.title}">
+const Header = `<header style="padding:1em;width:100%">
+    <div class="${title}">
         <span style="font-weight: bold;">Diagram</span>
         <span> as </span>
         <span style="font-weight: bold; font-style: italic;">Text</span>
