69590961f73d74e4a5c248a5d7f0076ee7f792ac
Dmitry Kisler <admin@dkisler.com>
2023-04-01 17:59:20 +0200

    chore(webclient): adjusts the header component's test; chore(webclient): fixed header component's test styling.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- webclient/test/header.test.ts
+++ webclient/test/header.test.ts
@@ -1,38 +1,52 @@
-import {describe, it} from 'vitest'
-import {equal} from "assert";
+import {assert, describe, it} from 'vitest'
 import {JSDOM} from 'jsdom';
 
 import Header from "./../src/components/header";
 
-describe('Header', () => {
-    //WHEN
+describe("Header component", () => {
     const got = new JSDOM(Header).window.document.getElementsByTagName("header");
 
-    //THEN
-    it('header tag', () => {
-        equal(got.length, 1, 'unexpected number of header elements');
+    it('shall include single header tag', () => {
+        assert.equal(got.length, 1)
     })
 
-    const header = got[0];
-    it('header style', () => {
-        equal(header.style.padding, "1em", 'unexpected header padding')
-        equal(header.style.width, "100%", 'unexpected header width')
+    const header = got[0]
+    it("shall have the padding of 1em", () => {
+        assert.equal(header.style.padding, "1em")
     })
 
-    const div = header.getElementsByTagName('div');
-    it('div tag', () => {
-        equal(div.length, 1, 'unexpected number of div children');
+    it("shall have the width of 100%", () => {
+        assert.equal(header.style.width, "100%")
     })
 
-    it('span tag', () => {
-        const span = div[0]!.getElementsByTagName("span");
-        equal(span[0]!.innerHTML, "Diagram", 'unexpected content of span[0]');
-        equal(span[0]!.style.fontWeight, "bold", 'unexpected font weight of the span[0]');
+    const div = header.getElementsByTagName('div')
+    it("shall have single div tag", () => {
+        assert.equal(div.length, 1)
+    })
 
-        equal(span[1]!.innerHTML, " as ", 'unexpected content of span[1]');
+    const span = div[0]!.getElementsByTagName("span")
+    it("shall have three span tags", () => {
+        assert.equal(span.length, 3)
+    })
 
-        equal(span[2]!.innerHTML, "Text", 'unexpected content of span[2]');
-        equal(span[2]!.style.fontWeight, "bold", 'unexpected font weight of the span[2]');
-        equal(span[2]!.style.fontStyle, "italic", 'unexpected font style of the span[2]');
+    it("shall have the span[0] with defined content", () => {
+        assert.equal(span[0]!.innerHTML, "Diagram")
+    })
+
+    it("shall have the span[0] with the font set to bold", () => {
+        assert.equal(span[0]!.style.fontWeight, "bold")
+    })
+
+    it("shall have the span[1] with defined content", () => {
+        assert.equal(span[1]!.innerHTML, " as ")
+    })
+
+    it("shall have the span[2] with defined content", () => {
+        assert.equal(span[2]!.innerHTML, "Text")
+    })
+
+    it("shall have the span[2] with the font set to bold italic", () => {
+        assert.equal(span[2]!.style.fontWeight, "bold")
+        assert.equal(span[2]!.style.fontStyle, "italic")
     })
 })
