493f895061c5eada846223d14c6063a7abdeefdb
Dmitry Kisler <admin@dkisler.com>
2023-04-02 11:39:50 +0200

    chore(webclient-footer): add social media icons to the footer component.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- webclient/src/components/footer.ts
+++ webclient/src/components/footer.ts
@@ -1,8 +1,39 @@
-export default function Footer(version: string): string {
-    const copyrightStr: string = `<p style="font-size:16px">diagramastext.dev &copy; ${new Date().getFullYear().toString()}`;
+// @ts-ignore
+import logoGithub from "./svg/github.svg";
+// @ts-ignore
+import logoSlack from "./svg/slack.svg";
+// @ts-ignore
+import logoLinkedin from "./svg/linkedin.svg";
+// @ts-ignore
+import logoEmail from "./svg/email.svg";
+
+
+export default function Footer(version: string = ""): string {
+    const copyrightStr =
+        `<p style="font-size:16px">diagramastext.dev &copy; ${new Date().getFullYear().toString()}</p>`;
+
     const versionElement = version === "" ? "" : `<p style="font-size:6px;">version: ${version}</p>`;
+
+    const iconSquareSize = 20;
+
+    const socialContact = `<p id="contacts" style="margin-top:-10px">
+    <a href="https://github.com/kislerdm/diagramastext">
+        <img src="${logoGithub}" width=${iconSquareSize} height=${iconSquareSize} alt="github"/>
+    </a>
+    <a href="https://join.slack.com/t/diagramastextdev/shared_invite/zt-1onedpbsz-ECNIfwjIj02xzBjWNGOllg">
+        <img src="${logoSlack}" width=${iconSquareSize} height=${iconSquareSize} alt="slack"/>
+    </a>
+    <a href="https://www.linkedin.com/in/dkisler">
+        <img src="${logoLinkedin}" width=${iconSquareSize} height=${iconSquareSize} alt="linkedin"/>
+    </a>
+    <a href="mailto:contact@diagramastext.dev">
+        <img src="${logoEmail}" width=${iconSquareSize} height=${iconSquareSize} alt="email"/>
+    </a>
+</p>`
+
     return `<footer style="padding:1rem">
     ${copyrightStr}
+    ${socialContact}
     ${versionElement}
 </footer>
 `
--- webclient/test/footer.test.ts
+++ webclient/test/footer.test.ts
@@ -2,65 +2,112 @@ import {assert, describe, it} from 'vitest'
 // @ts-ignore
 import {JSDOM} from 'jsdom';
 import Footer from "./../src/components/footer";
-import {equal} from "assert";
 
-describe('Footer component with version', () => {
+describe("Footer component", () => {
+    const got = new JSDOM(Footer()).window.document.getElementsByTagName("footer");
+
+    it("shall include single footer tag", () => {
+        assert.equal(got.length, 1)
+    })
+
+    it("shall have the padding of 1rem", () => {
+        assert.equal(got[0]!.style.padding, "1rem")
+    })
+
+    const pElements = got[0]!.getElementsByTagName("p");
+    it("shall have p tags", () => {
+        assert(pElements.length > 0)
+    })
+
+    const copyright = pElements[0];
+    it("shall include copyright which starts with the text diagramastext.dev", () => {
+        assert(copyright!.innerHTML.startsWith("diagramastext.dev"))
+    })
+
+    it("shall include copyright which ends with the current year", () => {
+        assert(copyright!.innerHTML.endsWith(`${new Date().getFullYear().toString()}`))
+    })
+
+    it("shall include copyright displayed as the text of 16px font size", () => {
+        assert.equal(copyright!.style.fontSize, "16px")
+    })
+
+    const socialContact = pElements[1];
+    it("shall include social medial links container", () => {
+        assert.equal(socialContact.id, "contacts")
+    })
+
+    it("shall include social medial links container with margins-top:-10px", () => {
+        assert.equal(socialContact.style.marginTop, "-10px")
+    })
+
+    const links = pElements[1].getElementsByTagName("a");
+    it("shall include social medial with four links", () => {
+        assert.equal(links.length, 4)
+    })
+
+    it("shall include link to github, leftmost", () => {
+        assert.equal(links[0].href, "https://github.com/kislerdm/diagramastext")
+    })
+
+    it("shall include link to slack, after github", () => {
+        assert(links[1].href.startsWith("https://join.slack.com"))
+    })
+
+    it("shall include link to linkedin, after slack", () => {
+        assert.equal(links[2].href, "https://www.linkedin.com/in/dkisler")
+    })
+
+    it("shall include link to email, after linkedin", () => {
+        assert.equal(links[3].href, "mailto:contact@diagramastext.dev")
+    })
+
+    const icons = pElements[1].getElementsByTagName("img");
+    it("shall include social medial icons as links", () => {
+        assert.equal(icons.length, 4)
+    })
+
+    it("shall include social medial icons: github is leftmost", () => {
+        assert.equal(icons[0].alt, "github")
+    })
+
+    it("shall include social medial icons: slack is after github", () => {
+        assert.equal(icons[1].alt, "slack")
+    })
+
+    it("shall include social medial icons: linkedin is after slack", () => {
+        assert.equal(icons[2].alt, "linkedin")
+    })
+
+    it("shall include social medial icons: email is after linkedin", () => {
+        assert.equal(icons[3].alt, "email")
+    })
+
+    it("shall include social medial icons with the width of 20px", () => {
+        for (let i = 0; i < icons.length; i++) {
+            assert.equal(icons[i].width, 20)
+        }
+    })
+})
+
+describe("Footer component with defined version", () => {
     //GIVEN
     const version = "foobar",
-        wantCopyrightFn = (s: string): boolean => s.startsWith("diagramastext.dev"),
         wantVersion = `version: ${version}`;
 
     //WHEN
     const got = Footer(version);
 
     //THEN
-    const footer = new JSDOM(got).window.document.getElementsByTagName("footer");
-    it('footer tag', () => {
-        equal(footer.length, 1, "single footer element is expected")
-        equal(footer[0].style.padding, "1rem", "unexpected footer padding")
+    const pElements = new JSDOM(got).window.document.getElementsByTagName("footer")[0]!
+            .getElementsByTagName("p"),
+        versionElement = pElements[pElements.length - 1];
+
+    it("shall include specified version beneath all elements", () => {
+        assert.equal(versionElement!.innerHTML, wantVersion)
     })
 
-    const pElements = footer[0].getElementsByTagName("p");
-    it('p tags - total count', () => {
-        equal(pElements.length, 2, "footer shall contain two p elements");
-    })
-
-    it('p tags - copyright', () => {
-        const copyright = pElements[0];
-        equal(copyright!.style.fontSize, "16px", "unexpected fontsize of the copyright string")
-        assert(wantCopyrightFn(copyright!.innerHTML), "unexpected copyright content")
-    })
-
-    it('p tags - version', () => {
-        const version = pElements[1];
-        equal(version!.style.fontSize, "6px", "unexpected fontsize of the version string")
-        equal(version!.innerHTML, wantVersion, "unexpected version string")
-    })
-})
-
-describe('Footer component without version', () => {
-    //GIVEN
-    const version = "",
-        wantCopyrightFn = (s: string): boolean => s.startsWith("diagramastext.dev");
-
-    //WHEN
-    const got = Footer(version);
-
-    //THEN
-    const footer = new JSDOM(got).window.document.getElementsByTagName("footer");
-    it('footer tag', () => {
-        equal(footer.length, 1, "single footer element is expected")
-        equal(footer[0].style.padding, "1rem", "unexpected footer padding")
-    })
-
-    const pElements = footer[0].getElementsByTagName("p");
-    it('p tags - total count', () => {
-        equal(pElements.length, 1, "footer shall contain two p elements");
-    })
-
-    it('p tags - copyright', () => {
-        const copyright = pElements[0];
-        equal(copyright!.style.fontSize, "16px", "unexpected fontsize of the copyright string")
-        assert(wantCopyrightFn(copyright!.innerHTML), "unexpected copyright content")
+    it("shall include the version displayed as the text of 6px font size", () => {
+        assert.equal(versionElement!.style.fontSize, "6px")
     })
 })
