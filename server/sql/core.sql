a724ffea842f379d8d85ed95dd08efcf51d23117
Dmitry Kisler <admin@dkisler.com>
2023-04-07 22:22:55 +0200

    chore(table): adds table to keep track of the users.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- server/sql/core.sql
+++ server/sql/core.sql
@@ -7,9 +7,33 @@ CREATE TABLE IF NOT EXISTS user_prompt (
 );
 
 CREATE TABLE IF NOT EXISTS openai_response (
-    id         SERIAL8,
-    request_id TEXT NOT NULL,
-    user_id    TEXT NOT NULL DEFAULT 'NA',
-    response   TEXT NOT NULL,
-    timestamp  TIMESTAMP NOT NULL DEFAULT NOW()
+    id                SERIAL8,
+    request_id        TEXT NOT NULL,
+    user_id           TEXT NOT NULL DEFAULT 'NA',
+    response          TEXT NOT NULL,
+    prompt_tokens     SMALLINT NOT NULL,
+    completion_tokens SMALLINT NOT NULL,
+    model             TEXT NOT NULL,
+    timestamp         TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE IF NOT EXISTS "user" (
+    id              UUID NOT NULL,
+    email           TEXT,
+    email_verified  BOOLEAN NOT NULL DEFAULT FALSE,
+    web_fingerprint TEXT,
+    is_active       BOOLEAN NOT NULL DEFAULT FALSE,
+    is_premium      BOOLEAN NOT NULL DEFAULT FALSE,
+    created_at      TIMESTAMP NOT NULL DEFAULT NOW(),
+    update_at       TIMESTAMP NOT NULL DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS user_api_token (
+   user_id      UUID NOT NULL,
+   token        UUID NOT NULL,
+   is_active    BOOLEAN NOT NULL DEFAULT TRUE,
+   last_used_at TIMESTAMP NOT NULL DEFAULT NOW(),
+   created_at   TIMESTAMP NOT NULL DEFAULT NOW()
 );
