0ecdd04cacc09a75bb4e578a3650bf890acd21ea
Dmitry Kisler <admin@dkisler.com>
2023-04-19 15:10:11 +0200

    feat(ciam): add jwt signing client which uses ed25519 key.
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

---
+++ server/core/httphandler/ciam/ed25519.go
@@ -0,0 +1,53 @@
+package ciam
+
+import (
+	"context"
+	"crypto"
+	"crypto/ed25519"
+	"errors"
+)
+
+// NewTokenSigningClientEd25519 initialises the client to sign JWT using ed25519 keys.
+func NewTokenSigningClientEd25519(privateKey, publicKey []byte) (TokenSigningClient, error) {
+	priv := ed25519.PrivateKey(privateKey)
+	pub := ed25519.PublicKey(publicKey)
+	if !pub.Equal(priv.Public()) {
+		return nil, errors.New("faulty private/public keys pair provided")
+	}
+	return clientEd25519{
+		alg:        "EdDSA",
+		privateKey: priv,
+		publicKey:  pub,
+	}, nil
+}
+
+type clientEd25519 struct {
+	privateKey ed25519.PrivateKey
+	publicKey  ed25519.PublicKey
+	alg        string
+}
+
+type hasherEd25519 struct{}
+
+func (h hasherEd25519) HashFunc() crypto.Hash {
+	return 0
+}
+
+func (e clientEd25519) Sign(_ context.Context, signingString string) (signature string, alg string, err error) {
+	signatureBytes, err := e.privateKey.Sign(nil, []byte(signingString), hasherEd25519{})
+
+	// The error is always nil because the signingString is ASSUMED to be not hashed - hasherEd25519 definition
+	// FIXME: would validation of hashing be required?
+	if err != nil {
+		return "", "", err
+	}
+
+	return string(signatureBytes), e.alg, nil
+}
+
+func (e clientEd25519) Verify(_ context.Context, signingString, signature string) error {
+	if !ed25519.Verify(e.publicKey, []byte(signingString), []byte(signature)) {
+		return errors.New("failed signature verification")
+	}
+	return nil
+}
