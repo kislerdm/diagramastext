6e087abc6d9966b4374793d1af9ce66cc688a7e5
Dmitry Kisler <admin@dkisler.com>
2023-09-05 11:28:28 +0200

    feat: add signin logic
    Signed-off-by: Dmitry Kisler <admin@dkisler.com>

--- webclient/src/ciam.ts
+++ webclient/src/ciam.ts
@@ -1,11 +1,7 @@
 import {getCookie, setCookie} from "typescript-cookie";
+import {v} from "msw/lib/glossary-de6278a9";
 
-const defaultNA = "NA";
-
-type jwt_header = {
-    alg: string;
-    typ: string;
-}
+const defaultNA: string = "";
 
 type user_quotas = {
     prompt_length_max: number;
@@ -13,212 +9,174 @@ type user_quotas = {
     rpd: number;
 }
 
-type jwt_payload = {
-    email_verified: string;
+const defaultQuotas = {
+    prompt_length_max: 100,
+    rpm: 1,
+    rpd: 1,
+};
+
+type claimsStd = {
+    sub: string;
+    exp: number;
+}
+
+type claimsRefresh = claimsStd;
+
+type claimsID = claimsStd & {
     email: string;
     fingerprint: string;
-    role: string;
+}
+
+type claimsAccess = claimsStd & {
+    role: number;
     quotas: user_quotas;
-    sub: string;
-    iss: string;
-    aud: string;
-    iat: bigint;
-    exp: bigint;
 }
-
-class JWT {
-    protected _default_aud: string = "https://diagramastext.dev";
-    protected _default_iss: string = "https://ciam.diagramastext.dev";
-
-    private readonly header: jwt_header;
-    private readonly payload: jwt_payload;
-    private readonly signature: string = "";
-
-    constructor(v: string) {
-        const el = v.split(".");
-
-        if (el.length < 2) {
-            throw new Error("invalid JWT")
-        }
-
-        this.header = JSON.parse(fromBase64(el[0]));
-        this.payload = JSON.parse(fromBase64(el[1]));
-
-        if (el.length < 3) {
-            this.signature = el[3];
-        }
-
-        this._validate()
-    }
-
-    isExpired(): boolean {
-        const nowSec = Date.now() / 1000;
-        return this.payload.exp < nowSec || this.payload.exp <= this.payload.iat;
-    }
-
-    serialize(): string {
-        let o = `${toBase64(JSON.stringify(this.header))}.${toBase64(JSON.stringify(this.payload))}`;
-        if (this.signature !== "") {
-            o += `.${this.signature}`
-        }
-        return o;
-    }
-
-    getQuotas(): user_quotas | undefined {
-        return this.payload.quotas;
-    }
-
-    getFingerprint(): string {
-        return this.payload.fingerprint;
-    }
-
-    getEmail(): string {
-        return this.payload.email;
-    }
-
-    getSub(): string {
-        return this.payload.sub;
-    }
-
-    private _validate() {
-        if (this.header.alg !== "none" && this.signature === "") {
-            throw new Error("invalid JWT: no signature found")
-        }
-        if (this.payload.aud !== this._default_aud) {
-            throw new Error("invalid JWT: faulty aud")
-        }
-        if (this.payload.iss !== this._default_iss) {
-            throw new Error("invalid JWT: faulty iss")
-        }
-    }
-}
-
-class Tokens {
-    identity: JWT;
-    access: JWT | null;
-    refresh: JWT | null;
-
-    constructor(rawTokensStr: string) {
-        const tkn: rawTokens = JSON.parse(rawTokensStr);
-
-        try {
-            this.identity = new JWT(tkn.identity);
-        } catch (e) {
-            // @ts-ignore
-            throw new Error(`identity token: ${e.message}`)
-        }
-
-        try {
-            this.access = this._parseToken(tkn.access);
-        } catch (e) {
-            // @ts-ignore
-            throw new Error(`access token: ${e.message}`)
-        }
-
-        try {
-            this.refresh = this._parseToken(tkn.refresh);
-        } catch (e) {
-            // @ts-ignore
-            throw new Error(`refresh token: ${e.message}`)
-        }
-    }
-
-    private _parseToken(v: string): JWT | null {
-        if (v !== "" && v !== null && v !== undefined) {
-            return new JWT(v);
-        }
-        return null;
-    }
-}
-
-type rawTokens = {
-    identity: string;
-    access: string;
-    refresh: string;
-};
 
 export function fromBase64(v: string): string {
     return Buffer.from(v, "base64").toString("binary");
 }
 
-export function toBase64(v: string): string {
-    return Buffer.from(v, "binary").toString("base64url");
+interface Tokens {
+    id: string | undefined;
+    access: string | undefined;
+    refresh: string | undefined;
+
+    setID(s: string): void;
+
+    setAccess(s: string): void;
+
+    setRefresh(s: string): void;
+
+    userID(): string | undefined;
+
+    quotas(): user_quotas | undefined;
+
+    stringify(): string;
+
+    isAuthUser(): boolean;
+}
+
+class tokens implements Tokens {
+    id: string | undefined;
+    access: string | undefined;
+    refresh: string | undefined;
+
+    private _id: claimsID | undefined;
+    private _access: claimsAccess | undefined;
+    private _refresh: claimsRefresh | undefined;
+
+    constructor(s: string | undefined) {
+        if (s === undefined || s === "") {
+            return
+        }
+
+        const tkns = JSON.parse(s);
+        if (tkns["id"] !== undefined) {
+            this.setID(tkns["id"]);
+        }
+
+        if (tkns["access"] !== undefined) {
+            this.setAccess(tkns["access"])
+        }
+
+        if (tkns["refresh"] !== undefined) {
+            this.setRefresh(tkns["refresh"])
+        }
+    }
+
+    setID(s: string): void {
+        this.id = s;
+        this._id = cast(this.parseJWTClaims(this.id), r("claimsID"));
+    }
+
+    setAccess(s: string): void {
+        this.access = s;
+        this._access = cast(this.parseJWTClaims(this.access), r("claimsAccess"));
+    }
+
+    setRefresh(s: string): void {
+        this.refresh = s;
+        this._refresh = cast(this.parseJWTClaims(this.refresh), r("claimsRefresh"));
+    }
+
+    stringify(): string {
+        return JSON.stringify({"id": this.id, "access": this.access, "refresh": this.refresh})
+    }
+
+    private parseJWTClaims(s: string): any {
+        const els = s.split(".");
+        if (els.length < 2) {
+            throw new Error("faulty JWT format")
+        }
+        return JSON.parse(fromBase64(els[1]));
+    }
+
+    quotas(): user_quotas {
+        if (this._access !== undefined) {
+            return this._access.quotas;
+        }
+        return defaultQuotas;
+    }
+
+    userID(): string | undefined {
+        return this._id?.sub;
+    }
+
+    isAuthUser(): boolean {
+        return this._access?.exp! > Date.now();
+    }
 }
 
 export class CIAMClient {
-    _cookie_tokens_key = "tokens";
-    _cookie_tokens_exp_days = 7;
+    _cookie_tokens_key: string = "tokens";
+    _cookie_tokens_exp_days: number = 100;
     private readonly _ciam_base_url: string;
 
     private readonly _fingerprint: string;
-    protected _tokens: Tokens | undefined;
-    readonly quotas: user_quotas;
 
-    email: string = "";
+    readonly tokens: tokens;
 
     constructor(ciam_base_url: string) {
         this._ciam_base_url = ciam_base_url;
-
-        const s = getCookie(this._cookie_tokens_key);
-        if (s !== undefined) {
-            this._tokens = new Tokens(s);
-            if (this._tokens.access?.isExpired()) {
-                this.refreshTokens()
-            }
-        }
-
-        this.quotas = this._tokens?.access?.getQuotas()!;
-
-        if (this.quotas === undefined) {
-            this.quotas = {
-                prompt_length_max: 100,
-                rpm: 1,
-                rpd: 1,
-            };
-        }
+        this.tokens = new tokens(getCookie(this._cookie_tokens_key));
 
         // TODO: add verification of the fingerprint
         // if the current fingerprint does not match the one in token
         // the CIAM shall be called to update the fingerprint and issue new tokens,
         // or to create a new anonym user
         // @ts-ignore
-        const userAgent = import.meta.env.DEV ? "NA" : navigator.userAgent;
+        const userAgent: string = import.meta.env.DEV ? "NA" : navigator.userAgent;
         this._fingerprint = get_fingerprint(userAgent);
     }
 
     isAuth(): boolean {
-        return this._tokens !== undefined;
+        return this.tokens.isAuthUser();
     }
 
     // Implements the logic to signin anonym user.
-    signInAnonym() {
-        throw Error("method mot implemented")
+    signInAnonym(): void {
+        fetch(`${this._ciam_base_url}/auth/anonym`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "fingerprint": this._fingerprint,
+            }),
+        }).then(resp => {
+            if (resp.status !== 200) {
+                throw new Error("error auth anonym")
+            }
+            return resp.json()
+        }).then(data => {
+            this.tokens.setID(data["id"])
+            this.tokens.setAccess(data["access"])
+            this.tokens.setRefresh(data["refresh"])
+        })
     }
 
-    // Implements the logic to initialise user registration.
-    registerInit(email: string) {
-        throw Error("method mot implemented")
-    }
-
-    // Implements the logic to confirm user registration.
-    registerConfirm(secret_code: string) {
-        throw Error("method mot implemented");
-    }
-
-    private refreshTokens() {
-        throw Error("method mot implemented");
-    }
-
-    isRegistered(): boolean {
-        return this._tokens?.identity.getEmail() !== "";
-    }
-
-    getId(): string {
-        return this._tokens?.identity.getSub()!;
-    }
-
-    private setTokensCache(ciamResponseTokens: string) {
-        setCookie(this._cookie_tokens_key, ciamResponseTokens, {
+    private setTokensCache(): void {
+        setCookie(this._cookie_tokens_key, this.tokens?.stringify(), {
             expires: this._cookie_tokens_exp_days,
             sameSite: "strict",
             secure: true,
@@ -226,7 +184,175 @@ export class CIAMClient {
             // TODO: add domain verification
         });
     }
+
+    getHeaderAccess(): Object {
+        if (this.tokens.access === "") {
+            return {}
+        }
+        return {
+            "Authorization": `Bearer ${this.tokens.access}`
+        }
+    }
+
+    getHeaderRefresh(): Object {
+        if (this.tokens.refresh === "") {
+            return {}
+        }
+        return {
+            "Authorization": `Bearer ${this.tokens.refresh}`
+        }
+    }
 }
+
+/**
+ * GENERATED by https://app.quicktype.io/
+ * */
+function invalidValue(typ: any, val: any, key: any, parent: any = ''): never {
+    const prettyTyp = prettyTypeName(typ);
+    const parentText = parent ? ` on ${parent}` : '';
+    const keyText = key ? ` for key "${key}"` : '';
+    throw Error(`Invalid value${keyText}${parentText}. Expected ${prettyTyp} but got ${JSON.stringify(val)}`);
+}
+
+function prettyTypeName(typ: any): string {
+    if (Array.isArray(typ)) {
+        if (typ.length === 2 && typ[0] === undefined) {
+            return `an optional ${prettyTypeName(typ[1])}`;
+        } else {
+            return `one of [${typ.map(a => {
+                return prettyTypeName(a);
+            }).join(", ")}]`;
+        }
+    } else if (typeof typ === "object" && typ.literal !== undefined) {
+        return typ.literal;
+    } else {
+        return typeof typ;
+    }
+}
+
+function jsonToJSProps(typ: any): any {
+    if (typ.jsonToJS === undefined) {
+        const map: any = {};
+        typ.props.forEach((p: any) => map[p.json] = {key: p.js, typ: p.typ});
+        typ.jsonToJS = map;
+    }
+    return typ.jsonToJS;
+}
+
+function transform(val: any, typ: any, getProps: any, key: any = '', parent: any = ''): any {
+    function transformPrimitive(typ: string, val: any): any {
+        if (typeof typ === typeof val) return val;
+        return invalidValue(typ, val, key, parent);
+    }
+
+    function transformUnion(typs: any[], val: any): any {
+        // val must validate against one typ in typs
+        const l = typs.length;
+        for (let i = 0; i < l; i++) {
+            const typ = typs[i];
+            try {
+                return transform(val, typ, getProps);
+            } catch (_) {
+            }
+        }
+        return invalidValue(typs, val, key, parent);
+    }
+
+    function transformEnum(cases: string[], val: any): any {
+        if (cases.indexOf(val) !== -1) return val;
+        return invalidValue(cases.map(a => {
+            return l(a);
+        }), val, key, parent);
+    }
+
+    function transformArray(typ: any, val: any): any {
+        // val must be an array with no invalid elements
+        if (!Array.isArray(val)) return invalidValue(l("array"), val, key, parent);
+        return val.map(el => transform(el, typ, getProps));
+    }
+
+    function transformDate(val: any): any {
+        if (val === null) {
+            return null;
+        }
+        const d = new Date(val);
+        if (isNaN(d.valueOf())) {
+            return invalidValue(l("Date"), val, key, parent);
+        }
+        return d;
+    }
+
+    function transformObject(props: { [k: string]: any }, additional: any, val: any): any {
+        if (val === null || typeof val !== "object" || Array.isArray(val)) {
+            return invalidValue(l(ref || "object"), val, key, parent);
+        }
+        const result: any = {};
+        Object.getOwnPropertyNames(props).forEach(key => {
+            const prop = props[key];
+            const v = Object.prototype.hasOwnProperty.call(val, key) ? val[key] : undefined;
+            result[prop.key] = transform(v, prop.typ, getProps, key, ref);
+        });
+        Object.getOwnPropertyNames(val).forEach(key => {
+            if (!Object.prototype.hasOwnProperty.call(props, key)) {
+                result[key] = transform(val[key], additional, getProps, key, ref);
+            }
+        });
+        return result;
+    }
+
+    if (typ === "any") return val;
+    if (typ === null) {
+        if (val === null) return val;
+        return invalidValue(typ, val, key, parent);
+    }
+    if (typ === false) return invalidValue(typ, val, key, parent);
+    let ref: any = undefined;
+    while (typeof typ === "object" && typ.ref !== undefined) {
+        ref = typ.ref;
+        typ = typeMap[typ.ref];
+    }
+    if (Array.isArray(typ)) return transformEnum(typ, val);
+    if (typeof typ === "object") {
+        return typ.hasOwnProperty("unionMembers") ? transformUnion(typ.unionMembers, val)
+            : typ.hasOwnProperty("arrayItems") ? transformArray(typ.arrayItems, val)
+                : typ.hasOwnProperty("props") ? transformObject(getProps(typ), typ.additional, val)
+                    : invalidValue(typ, val, key, parent);
+    }
+    // Numbers can be parsed by Date but shouldn't be.
+    if (typ === Date && typeof val !== "number") return transformDate(val);
+    return transformPrimitive(typ, val);
+}
+
+function cast<T>(val: any, typ: any): T {
+    return transform(val, typ, jsonToJSProps);
+}
+
+function l(typ: any) {
+    return {literal: typ};
+}
+
+function a(typ: any) {
+    return {arrayItems: typ};
+}
+
+function u(...typs: any[]) {
+    return {unionMembers: typs};
+}
+
+function o(props: any[], additional: any) {
+    return {props, additional};
+}
+
+function r(name: string) {
+    return {ref: name};
+}
+
+const typeMap: any = {
+    "Graph": o([
+        {json: "links", js: "links", typ: u(undefined, a(r("Link")))},
+        {json: "nodes", js: "nodes", typ: a(r("Node"))},
+    ], "any"),
+};
 
 /**
  * Generates a user's fingerprint to identify the session.
